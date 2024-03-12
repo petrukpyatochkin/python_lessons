719945f2b173552d1a563441873a405e8b37d714
Iaroslav <iaroslav.kernytsksyi@gmail.com>
2024-03-12 19:31:11 +0200

    Second commit

---
+++ animal.py
@@ -0,0 +1,15 @@
+class Animal:
+
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
+
+    def breaf(self):
+        print("breaf")
+
+    def move(self):
+        print("move")
+
+    def eat(self):
+        print("eat")
---
+++ main.py
@@ -0,0 +1,4 @@
+from dog import Dog
+
+dog = Dog()
+dog.move()
