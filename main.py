9fc6ba5cf1eff1cc6d059d9b02b04879be22e369
Iaroslav <iaroslav.kernytsksyi@gmail.com>
2024-03-12 20:17:26 +0200

    lab_25_task_2

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from lab_25_task_1.circle import Circle
 from lab_25_task_1.student import Student
 
 student = Student("Іван", 20)
 student.print_info()
+
+circle = Circle(10)
+print(circle.area())
